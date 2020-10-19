d3b252274154947df4dafe676e356c0a966c388e
Wali Queiroz <wsantos077@gmail.com>
2020-10-19 09:26:19 -0300

    Abstração de componentes

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
+import { FiArrowRight, FiPlus } from 'react-icons/fi';
 import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css'
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import mapIcon from '../utils/mapIcon';
 
 function OrphanagesMap() {
     return (
@@ -30,9 +31,22 @@ function OrphanagesMap() {
             >
                 {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
                 <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+
+                <Marker
+                    position={[-13.8403715, -40.0735516]}
+                    icon={mapIcon}
+                >
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                        Lar das meninas
+                        <Link to="/orphanages/1">
+                            <FiArrowRight size={20} color="#fff" />
+                        </Link>
+                    </Popup>
+                </Marker>
+
             </Map>
 
-            <Link to="" className="create-orphanage">
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
             </Link>
         </div>
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
+import CreateOrphanage from './pages/CreateOrphanage';
+import Orphanage from './pages/Orphanage';
 
 function Routes() {
     return (
@@ -9,6 +11,8 @@ function Routes() {
             <Switch>
                 <Route path="/" exact component={Landing}></Route>
                 <Route path="/app" component={OrphanagesMap}></Route>
+                <Route path="/orphanages/create" component={CreateOrphanage}></Route>
+                <Route path="/orphanages/:id" component={Orphanage}></Route>
             </Switch>
         </BrowserRouter>
     );
